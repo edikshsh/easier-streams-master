64d48d3cc2c65b753120c5e261399985d8c8e395
edikshsh <edikshsh@gmail.com>
2023-01-19 21:03:51 +0200

    rename pipe-helper to plumber

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { objectTransformsHelper, TransformsHelper, transformsHelper } from './st
 import { SimpleAsyncTransform } from './streams/transforms/base/simple-async-transform';
 import { SimpleTransform } from './streams/transforms/base/simple-transform';
 import { getStreamPipe } from './streams/stream-pipe';
-import { pipeHelper } from './streams/pipe-helper';
+import { plumber } from './streams/plumber';
 import { BaseTransform } from './streams/transforms/base/base-transform';
 import { TypedTransform } from './streams/transforms/typed-transform/typed-transform.interface';
 import { ArrayJoinTransform } from './streams/transforms/utility/array-join-transform';
@@ -31,7 +31,7 @@ export {
     transformsHelper,
     objectTransformsHelper,
     TransformsHelper,
-    pipeHelper,
+    plumber,
     StreamPipe,
     SimpleTransform,
     SimpleAsyncTransform,
